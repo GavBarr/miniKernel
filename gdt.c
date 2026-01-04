2d83fb43b59f04985e953199cc9238555982db96
GavinBarrett <gavinbarrett2001@gmail.com>
2026-01-04 13:00:10 -0700

    adding logic to add entries to the gdt

---
+++ gdt.c
@@ -0,0 +1,40 @@
+#include "gdt.h"
+
+
+struct gdt_entry gdt[3];
+//0 - Null Descriptor
+//1 - Kernel code segment descriptor
+//2 - Kernel data segment descriptor
+
+
+void gdt_init(void){
+	//dont need to do this for [0] because it should all be NULL or rather 0
+	gdt_add_entry(1, 0, 0xFFFFF, 0x9A, 0xCF);
+	gdt_add_entry(2, 0, 0xFFFFF, 0x92, 0xCF);
+
+}
+
+void gdt_add_entry(int entry_index, uint32_t base, uint32_t limit, uint8_t access, uint8_t flags){
+
+	//base_low is 16bits
+	//middle is 8 bits
+	//high is 8 bits
+	//Bit position: 7  6  5  4  3  2  1  0
+        //  	       [  ][  ][  ][  ][  ][  ][  ][  ]
+        //    	       ↑                             ↑
+        //             "high"                        "low"
+        //             (most significant)        (least significant)
+	
+
+	gdt[entry_index].base_low = base & 0xFFFF;
+	gdt[entry_index].base_middle = (base >> 16) & 0xFF;
+	gdt[entry_index].base_high = (base >> 24) & 0xFF;
+
+	gdt[entry_index].limit_low = limit & 0xFFFF;
+	uint8_t limit_high = (limit >> 16) & 0x0F;
+
+	gdt[entry_index].granularity = ( (limit_high & 0x0F) | (flags & 0x0F));
+	gdt[entry_index].access = access;
+
+
+}
--- gdt.h
+++ gdt.h
@@ -1,16 +1,29 @@
 #ifndef GDT_H
 #define GDT_H
+#include <stdint.h>
 
-
-struct gdt{
+//MUST be 8bytes, the order of the bytes matter significantly and must match this
+struct gdt_entry{
+	
+	uint16_t limit_low;
+	uint16_t base_low;
+	uint8_t base_middle;
+	uint8_t access;
+	uint8_t granularity;
+	uint8_t base_high;
 
 
 };
 
+struct gdt_ptr{
 
-int gdt_init(struct gdt *g){
+	uint16_t limit;
+	uint32_t base;
 
-}
+};
+
+
+void gdt_init(void);
 
 #endif
 
